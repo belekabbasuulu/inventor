9e271f77911149642f117f188909f2ec10f5cee0
belekabbasuulu <djonwick98@gmail.com>
2021-08-10 20:58:07 +0600

    add adding

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from openpyxl import load_workbook, Workbook
+
+
+excel = load_workbook('report.xlsx')
+
+page = excel['Sheet']
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	f = open('goods.txt', 'r', encoding = 'utf-8')
-	txt = f.readlines()
+	txt = []
+	print(len(page['A']))
+	for i in range(1,len(page['A'])+1):
+		txt.append(page["A"+str(i)].value)
 	return render_template('index.html', goods=txt)
 	
+
+@app.route('/add/', methods=['POST'])
+def add():
+	good = request.form['good']
+	page['A'+str(len(page['A'])+1)] = good
+	excel.save('report.xlsx')
+	return """ 
+		<h1> Инвентарь пополнен" </h1>
+		<a href='/'> Домой </a>
+	"""
